pymc-devs/pymc

Remove shape dependencies from DictToArrayBijection
This commit changes `DictToArrayBijection` so that it returns a `RaveledVars`
datatype that contains the original raveled and concatenated vector along with
the information needed to revert it back to dictionay/variables form.

Simply put, the variables-to-single-vector mapping steps have been pushed away
from the model object and its symbolic terms and closer to the (sampling)
processes that produce and work with `ndarray` values for said terms.  In doing
so, we can operate under fewer unnecessarily strong assumptions (e.g. that the
shapes of each term are static and equal to the initial test points), and let
the sampling processes that require vector-only steps deal with any changes in
the mappings.

pymc3/blocking.py
@@ -18,14 +18,14 @@ pymc3.blocking
 Classes for working with subsets of parameters.
 """
 import collections
-import copy
+
+from typing import Dict, List, Text, Union
 
 import numpy as np
 
-from pymc3.util import get_var_name
+__all__ = ["ArrayOrdering", "DictToArrayBijection"]
 
-__all__ = ["ArrayOrdering", "DictToArrayBijection", "DictToVarBijection"]
-
+RaveledVars = collections.namedtuple("RaveledVars", "data, point_map_info")
 VarMap = collections.namedtuple("VarMap", "var, slc, shp, dtyp")
 DataMap = collections.namedtuple("DataMap", "list_ind, slc, shp, dtype, name")
 
@@ -67,19 +67,11 @@ class DictToArrayBijection:
     A mapping between a dict space and an array space
     """
 
-    def __init__(self, ordering, dpoint):
+    def __init__(self, ordering: List[Text]):
+        # TODO: Should just use `OrderedDict`s and remove this state entirely
         self.ordering = ordering
-        self.dpt = dpoint
 
-        # determine smallest float dtype that will fit all data
-        if all([x.dtyp == "float16" for x in ordering.vmap]):
-            self.array_dtype = "float16"
-        elif all([x.dtyp == "float32" for x in ordering.vmap]):
-            self.array_dtype = "float32"
-        else:
-            self.array_dtype = "float64"
-
-    def map(self, dpt):
+    def map(self, dpt: Dict[Text, np.ndarray]):
         """
         Maps value from dict space to array space
 
@@ -87,12 +79,14 @@ class DictToArrayBijection:
         ----------
         dpt: dict
         """
-        apt = np.empty(self.ordering.size, dtype=self.array_dtype)
-        for var, slc, _, _ in self.ordering.vmap:
-            apt[slc] = dpt[var].ravel()
-        return apt
+        vars_info = tuple((dpt[n], n, dpt[n].shape, dpt[n].dtype) for n in self.ordering)
+        res = np.concatenate([v[0].ravel() for v in vars_info])
+        return RaveledVars(res, tuple(v[1:] for v in vars_info))
 
-    def rmap(self, apt):
+    @classmethod
+    def rmap(
+        cls, apt: RaveledVars, as_list=False
+    ) -> Union[Dict[Text, np.ndarray], List[np.ndarray]]:
         """
         Maps value from array space to dict space
 
@@ -100,12 +94,25 @@ class DictToArrayBijection:
         ----------
         apt: array
         """
-        dpt = self.dpt.copy()
+        if as_list:
+            res = []
+        else:
+            res = {}
 
-        for var, slc, shp, dtyp in self.ordering.vmap:
-            dpt[var] = np.atleast_1d(apt)[slc].reshape(shp).astype(dtyp)
+        if not isinstance(apt, RaveledVars):
+            raise TypeError("`apt` must be a `RaveledVars` type")
 
-        return dpt
+        last_idx = 0
+        for name, shape, dtype in apt.point_map_info:
+            arr_len = np.prod(shape, dtype=int)
+            var = apt.data[last_idx : last_idx + arr_len].reshape(shape).astype(dtype)
+            if as_list:
+                res.append(var)
+            else:
+                res[name] = var
+            last_idx += arr_len
+
+        return res
 
     def mapf(self, f):
         """
@@ -120,142 +127,6 @@ class DictToArrayBijection:
         -------
         f: array -> T
         """
-        return Compose(f, self.rmap)
-
-
-class ListArrayOrdering:
-    """
-    An ordering for a list to an array space. Takes also non theano.tensors.
-    Modified from pymc3 blocking.
-
-    Parameters
-    ----------
-    list_arrays: list
-        :class:`numpy.ndarray` or :class:`theano.tensor.Tensor`
-    intype: str
-        defining the input type 'tensor' or 'numpy'
-    """
-
-    def __init__(self, list_arrays, intype="numpy"):
-        if intype not in {"tensor", "numpy"}:
-            raise ValueError("intype not in {'tensor', 'numpy'}")
-        self.vmap = []
-        self.intype = intype
-        self.size = 0
-        for array in list_arrays:
-            if self.intype == "tensor":
-                name = array.name
-                array = array.tag.test_value
-            else:
-                name = "numpy"
-
-            slc = slice(self.size, self.size + array.size)
-            self.vmap.append(DataMap(len(self.vmap), slc, array.shape, array.dtype, name))
-            self.size += array.size
-
-
-class ListToArrayBijection:
-    """
-    A mapping between a List of arrays and an array space
-
-    Parameters
-    ----------
-    ordering: :class:`ListArrayOrdering`
-    list_arrays: list
-        of :class:`numpy.ndarray`
-    """
-
-    def __init__(self, ordering, list_arrays):
-        self.ordering = ordering
-        self.list_arrays = list_arrays
-
-    def fmap(self, list_arrays):
-        """
-        Maps values from List space to array space
-
-        Parameters
-        ----------
-        list_arrays: list
-            of :class:`numpy.ndarray`
-
-        Returns
-        -------
-        array: :class:`numpy.ndarray`
-            single array comprising all the input arrays
-        """
-
-        array = np.empty(self.ordering.size)
-        for list_ind, slc, _, _, _ in self.ordering.vmap:
-            array[slc] = list_arrays[list_ind].ravel()
-        return array
-
-    def dmap(self, dpt):
-        """
-        Maps values from dict space to List space
-
-        Parameters
-        ----------
-        list_arrays: list
-            of :class:`numpy.ndarray`
-
-        Returns
-        -------
-        point
-        """
-        a_list = copy.copy(self.list_arrays)
-
-        for list_ind, _, _, _, var in self.ordering.vmap:
-            a_list[list_ind] = dpt[var].ravel()
-
-        return a_list
-
-    def rmap(self, array):
-        """
-        Maps value from array space to List space
-        Inverse operation of fmap.
-
-        Parameters
-        ----------
-        array: :class:`numpy.ndarray`
-
-        Returns
-        -------
-        a_list: list
-            of :class:`numpy.ndarray`
-        """
-
-        a_list = copy.copy(self.list_arrays)
-
-        for list_ind, slc, shp, dtype, _ in self.ordering.vmap:
-            a_list[list_ind] = np.atleast_1d(array)[slc].reshape(shp).astype(dtype)
-
-        return a_list
-
-
-class DictToVarBijection:
-    """
-    A mapping between a dict space and the array space for one element within the dict space
-    """
-
-    def __init__(self, var, idx, dpoint):
-        self.var = get_var_name(var)
-        self.idx = idx
-        self.dpt = dpoint
-
-    def map(self, dpt):
-        return dpt[self.var][self.idx]
-
-    def rmap(self, apt):
-        dpt = self.dpt.copy()
-
-        dvar = dpt[self.var].copy()
-        dvar[self.idx] = apt
-
-        dpt[self.var] = dvar
-
-        return dpt
-
-    def mapf(self, f):
         return Compose(f, self.rmap)
 
 

pymc3/step_methods/hmc/base_hmc.py
@@ -20,12 +20,13 @@ from collections import namedtuple
 import numpy as np
 
 from pymc3.backends.report import SamplerWarning, WarningType
+from pymc3.blocking import RaveledVars
 from pymc3.exceptions import SamplingError
 from pymc3.model import Point, modelcontext
 from pymc3.step_methods import arraystep, step_sizes
 from pymc3.step_methods.hmc import integration
 from pymc3.step_methods.hmc.quadpotential import QuadPotentialDiagAdapt, quad_potential
-from pymc3.theanof import floatX, inputvars
+from pymc3.theanof import floatX
 from pymc3.tuning import guess_scaling
 
 logger = logging.getLogger("pymc3")
@@ -83,7 +84,8 @@ class BaseHMC(arraystep.GradientSharedStep):
 
         if vars is None:
             vars = self._model.cont_vars
-        vars = inputvars(vars)
+
+        # vars = inputvars(vars)
 
         super().__init__(vars, blocked=blocked, model=model, dtype=dtype, **theano_kwargs)
 
@@ -93,7 +95,7 @@ class BaseHMC(arraystep.GradientSharedStep):
 
         # We're using the initial/test point to determine the (initial) step
         # size.
-        # TODO: If the dimensions of these terms change, the step size
+        # XXX: If the dimensions of these terms change, the step size
         # dimension-scaling should change as well, no?
         test_point = model.test_point
         continuous_vars = [test_point[v.name] for v in model.cont_vars]
@@ -143,6 +145,8 @@ class BaseHMC(arraystep.GradientSharedStep):
         process_start = time.process_time()
 
         p0 = self.potential.random()
+        p0 = RaveledVars(p0, q0.point_map_info)
+
         start = self.integrator.compute_state(q0, p0)
 
         if not np.isfinite(start.energy):
@@ -151,7 +155,7 @@ class BaseHMC(arraystep.GradientSharedStep):
             error_logp = check_test_point.loc[
                 (np.abs(check_test_point) >= 1e20) | np.isnan(check_test_point)
             ]
-            self.potential.raise_ok(self._logp_dlogp_func._ordering.vmap)
+            self.potential.raise_ok(q0.point_map_info)
             message_energy = (
                 "Bad initial energy, check any log probabilities that "
                 "are inf or -inf, nan or very small:\n{}".format(error_logp.to_string())
@@ -172,7 +176,7 @@ class BaseHMC(arraystep.GradientSharedStep):
         if self._step_rand is not None:
             step_size = self._step_rand(step_size)
 
-        hmc_step = self._hamiltonian_step(start, p0, step_size)
+        hmc_step = self._hamiltonian_step(start, p0.data, step_size)
 
         perf_end = time.perf_counter()
         process_end = time.process_time()
@@ -191,9 +195,11 @@ class BaseHMC(arraystep.GradientSharedStep):
                 self._num_divs_sample += 1
                 # We don't want to fill up all memory with divergence info
                 if self._num_divs_sample < 100 and info.state is not None:
-                    point = self._logp_dlogp_func.array_to_dict(info.state.q)
+                    point = self.bij.rmap(info.state.q)
+
                 if self._num_divs_sample < 100 and info.state_div is not None:
-                    point_dest = self._logp_dlogp_func.array_to_dict(info.state_div.q)
+                    point = self.bij.rmap(info.state_div.q)
+
                 if self._num_divs_sample < 100:
                     info_store = info
             warning = SamplerWarning(

arkwave/pymc3

pymc3/blocking.py
@@ -18,19 +18,18 @@ pymc3.blocking
 Classes for working with subsets of parameters.
 """
 import collections
-import copy
+
+from typing import Dict, List, Optional, Union
 
 import numpy as np
 
-from pymc3.util import get_var_name
+__all__ = ["ArrayOrdering", "DictToArrayBijection"]
 
-__all__ = ["ArrayOrdering", "DictToArrayBijection", "DictToVarBijection"]
-
+# `point_map_info` is a tuple of tuples containing `(name, shape, dtype)` for
+# each of the raveled variables.
+RaveledVars = collections.namedtuple("RaveledVars", "data, point_map_info")
 VarMap = collections.namedtuple("VarMap", "var, slc, shp, dtyp")
 DataMap = collections.namedtuple("DataMap", "list_ind, slc, shp, dtype, name")
-
-
-# TODO Classes and methods need to be fully documented.
 
 
 class ArrayOrdering:
@@ -63,200 +62,67 @@ class ArrayOrdering:
 
 
 class DictToArrayBijection:
-    """
-    A mapping between a dict space and an array space
+    """Map between a `dict`s of variables to an array space.
+
+    Said array space consists of all the vars raveled and then concatenated.
+
     """
 
-    def __init__(self, ordering, dpoint):
-        self.ordering = ordering
-        self.dpt = dpoint
+    @staticmethod
+    def map(var_dict: Dict[str, np.ndarray]) -> RaveledVars:
+        """Map a dictionary of names and variables to a concatenated 1D array space."""
+        vars_info = tuple((v, k, v.shape, v.dtype) for k, v in var_dict.items())
+        res = np.concatenate([v[0].ravel() for v in vars_info])
+        return RaveledVars(res, tuple(v[1:] for v in vars_info))
 
-        # determine smallest float dtype that will fit all data
-        if all([x.dtyp == "float16" for x in ordering.vmap]):
-            self.array_dtype = "float16"
-        elif all([x.dtyp == "float32" for x in ordering.vmap]):
-            self.array_dtype = "float32"
+    @staticmethod
+    def rmap(
+        array: RaveledVars, as_list: Optional[bool] = False
+    ) -> Union[Dict[str, np.ndarray], List[np.ndarray]]:
+        """Map 1D concatenated array to a dictionary of variables in their original spaces.
+
+        Parameters
+        ==========
+        array
+            The array to map.
+        as_list
+            When ``True``, return a list of the original variables instead of a
+            ``dict`` keyed each variable's name.
+        """
+        if as_list:
+            res = []
         else:
-            self.array_dtype = "float64"
+            res = {}
 
-    def map(self, dpt):
-        """
-        Maps value from dict space to array space
+        if not isinstance(array, RaveledVars):
+            raise TypeError("`apt` must be a `RaveledVars` type")
 
-        Parameters
-        ----------
-        dpt: dict
-        """
-        apt = np.empty(self.ordering.size, dtype=self.array_dtype)
-        for var, slc, _, _ in self.ordering.vmap:
-            apt[slc] = dpt[var].ravel()
-        return apt
+        last_idx = 0
+        for name, shape, dtype in array.point_map_info:
+            arr_len = np.prod(shape, dtype=int)
+            var = array.data[last_idx : last_idx + arr_len].reshape(shape).astype(dtype)
+            if as_list:
+                res.append(var)
+            else:
+                res[name] = var
+            last_idx += arr_len
 
-    def rmap(self, apt):
-        """
-        Maps value from array space to dict space
+        return res
 
-        Parameters
-        ----------
-        apt: array
-        """
-        dpt = self.dpt.copy()
-
-        for var, slc, shp, dtyp in self.ordering.vmap:
-            dpt[var] = np.atleast_1d(apt)[slc].reshape(shp).astype(dtyp)
-
-        return dpt
-
-    def mapf(self, f):
+    @classmethod
+    def mapf(cls, f):
         """
          function f: DictSpace -> T to ArraySpace -> T
 
         Parameters
         ----------
-
         f: dict -> T
 
         Returns
         -------
         f: array -> T
         """
-        return Compose(f, self.rmap)
-
-
-class ListArrayOrdering:
-    """
-    An ordering for a list to an array space. Takes also non aesara.tensors.
-    Modified from pymc3 blocking.
-
-    Parameters
-    ----------
-    list_arrays: list
-        :class:`numpy.ndarray` or :class:`aesara.tensor.Tensor`
-    intype: str
-        defining the input type 'tensor' or 'numpy'
-    """
-
-    def __init__(self, list_arrays, intype="numpy"):
-        if intype not in {"tensor", "numpy"}:
-            raise ValueError("intype not in {'tensor', 'numpy'}")
-        self.vmap = []
-        self.intype = intype
-        self.size = 0
-        for array in list_arrays:
-            if self.intype == "tensor":
-                name = array.name
-                array = array.tag.test_value
-            else:
-                name = "numpy"
-
-            slc = slice(self.size, self.size + array.size)
-            self.vmap.append(DataMap(len(self.vmap), slc, array.shape, array.dtype, name))
-            self.size += array.size
-
-
-class ListToArrayBijection:
-    """
-    A mapping between a List of arrays and an array space
-
-    Parameters
-    ----------
-    ordering: :class:`ListArrayOrdering`
-    list_arrays: list
-        of :class:`numpy.ndarray`
-    """
-
-    def __init__(self, ordering, list_arrays):
-        self.ordering = ordering
-        self.list_arrays = list_arrays
-
-    def fmap(self, list_arrays):
-        """
-        Maps values from List space to array space
-
-        Parameters
-        ----------
-        list_arrays: list
-            of :class:`numpy.ndarray`
-
-        Returns
-        -------
-        array: :class:`numpy.ndarray`
-            single array comprising all the input arrays
-        """
-
-        array = np.empty(self.ordering.size)
-        for list_ind, slc, _, _, _ in self.ordering.vmap:
-            array[slc] = list_arrays[list_ind].ravel()
-        return array
-
-    def dmap(self, dpt):
-        """
-        Maps values from dict space to List space
-
-        Parameters
-        ----------
-        list_arrays: list
-            of :class:`numpy.ndarray`
-
-        Returns
-        -------
-        point
-        """
-        a_list = copy.copy(self.list_arrays)
-
-        for list_ind, _, _, _, var in self.ordering.vmap:
-            a_list[list_ind] = dpt[var].ravel()
-
-        return a_list
-
-    def rmap(self, array):
-        """
-        Maps value from array space to List space
-        Inverse operation of fmap.
-
-        Parameters
-        ----------
-        array: :class:`numpy.ndarray`
-
-        Returns
-        -------
-        a_list: list
-            of :class:`numpy.ndarray`
-        """
-
-        a_list = copy.copy(self.list_arrays)
-
-        for list_ind, slc, shp, dtype, _ in self.ordering.vmap:
-            a_list[list_ind] = np.atleast_1d(array)[slc].reshape(shp).astype(dtype)
-
-        return a_list
-
-
-class DictToVarBijection:
-    """
-    A mapping between a dict space and the array space for one element within the dict space
-    """
-
-    def __init__(self, var, idx, dpoint):
-        self.var = get_var_name(var)
-        self.idx = idx
-        self.dpt = dpoint
-
-    def map(self, dpt):
-        return dpt[self.var][self.idx]
-
-    def rmap(self, apt):
-        dpt = self.dpt.copy()
-
-        dvar = dpt[self.var].copy()
-        dvar[self.idx] = apt
-
-        dpt[self.var] = dvar
-
-        return dpt
-
-    def mapf(self, f):
-        return Compose(f, self.rmap)
+        return Compose(f, cls.rmap)
 
 
 class Compose:

pymc3/step_methods/hmc/base_hmc.py
@@ -19,8 +19,9 @@ from collections import namedtuple
 
 import numpy as np
 
-from pymc3.aesaraf import floatX, inputvars
+from pymc3.aesaraf import floatX
 from pymc3.backends.report import SamplerWarning, WarningType
+from pymc3.blocking import DictToArrayBijection, RaveledVars
 from pymc3.exceptions import SamplingError
 from pymc3.model import Point, modelcontext
 from pymc3.step_methods import arraystep, step_sizes
@@ -83,7 +84,8 @@ class BaseHMC(arraystep.GradientSharedStep):
 
         if vars is None:
             vars = self._model.cont_vars
-        vars = inputvars(vars)
+
+        # vars = inputvars(vars)
 
         super().__init__(vars, blocked=blocked, model=self._model, dtype=dtype, **aesara_kwargs)
 
@@ -93,7 +95,7 @@ class BaseHMC(arraystep.GradientSharedStep):
 
         # We're using the initial/test point to determine the (initial) step
         # size.
-        # TODO: If the dimensions of these terms change, the step size
+        # XXX: If the dimensions of these terms change, the step size
         # dimension-scaling should change as well, no?
         test_point = self._model.test_point
         continuous_vars = [test_point[v.name] for v in self._model.cont_vars]
@@ -143,6 +145,8 @@ class BaseHMC(arraystep.GradientSharedStep):
         process_start = time.process_time()
 
         p0 = self.potential.random()
+        p0 = RaveledVars(p0, q0.point_map_info)
+
         start = self.integrator.compute_state(q0, p0)
 
         if not np.isfinite(start.energy):
@@ -151,7 +155,7 @@ class BaseHMC(arraystep.GradientSharedStep):
             error_logp = check_test_point.loc[
                 (np.abs(check_test_point) >= 1e20) | np.isnan(check_test_point)
             ]
-            self.potential.raise_ok(self._logp_dlogp_func._ordering.vmap)
+            self.potential.raise_ok(q0.point_map_info)
             message_energy = (
                 "Bad initial energy, check any log probabilities that "
                 "are inf or -inf, nan or very small:\n{}".format(error_logp.to_string())
@@ -172,7 +176,7 @@ class BaseHMC(arraystep.GradientSharedStep):
         if self._step_rand is not None:
             step_size = self._step_rand(step_size)
 
-        hmc_step = self._hamiltonian_step(start, p0, step_size)
+        hmc_step = self._hamiltonian_step(start, p0.data, step_size)
 
         perf_end = time.perf_counter()
         process_end = time.process_time()
@@ -191,9 +195,11 @@ class BaseHMC(arraystep.GradientSharedStep):
                 self._num_divs_sample += 1
                 # We don't want to fill up all memory with divergence info
                 if self._num_divs_sample < 100 and info.state is not None:
-                    point = self._logp_dlogp_func.array_to_dict(info.state.q)
+                    point = DictToArrayBijection.rmap(info.state.q)
+
                 if self._num_divs_sample < 100 and info.state_div is not None:
-                    point_dest = self._logp_dlogp_func.array_to_dict(info.state_div.q)
+                    point = DictToArrayBijection.rmap(info.state_div.q)
+
                 if self._num_divs_sample < 100:
                     info_store = info
             warning = SamplerWarning(